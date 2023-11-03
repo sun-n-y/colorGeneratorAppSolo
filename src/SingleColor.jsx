c33c04a16d260dc5cb6c8066ac6b71ec6bc40bef
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-03 13:03:01 -0400

    add save to clipboard

--- src/SingleColor.jsx
+++ src/SingleColor.jsx
@@ -1,8 +1,22 @@
 const SingleColor = ({ color, index }) => {
   const { hex, weight } = color;
 
+  const saveToClipBoard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
-    <article className="single-color" style={{ background: `#${hex}` }}>
+    <article
+      className="single-color"
+      style={{ background: `#${hex}` }}
+      onClick={saveToClipBoard}
+    >
       <h4 className={index > 10 ? 'white-font' : ''}>{weight}%</h4>
       <h4 className={index > 10 ? 'white-font' : ''}>#{hex}</h4>
     </article>
